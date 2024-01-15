525acf5bb3e5ce4b37df2309f2a58cf71333638c
alexjplant <alex@alexplant.org>
2024-01-15 12:18:51 -0800

    Computing type matchups now

--- src/domains/monsters/components/SidebarDetail.tsx
+++ src/domains/monsters/components/SidebarDetail.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "preact/hooks";
 import { State } from "../../../state";
 import { Monster } from "../data/Monster";
+import { GetTypeMatchupMultiplier } from "../../types/data/TypeMatchup";
 
 export interface SidebarDetailProps {
     monster: Monster;
@@ -8,13 +9,48 @@ export interface SidebarDetailProps {
 
 export const SidebarDetail = (props: SidebarDetailProps) => {
     const state = useContext(State);
-    const attackingMatchups = state.data.typeMatchups.value.filter((typeMatchup) => typeMatchup.defendingType.includes(props.monster.type1)
-        || typeMatchup.defendingType.includes(props.monster.type2)).map(matchup => `${matchup.effectiveness} ${matchup.defendingType}`);
+    const defendingMatchups = state.data.typeMatchups.value.filter((typeMatchup) => typeMatchup.defendingType === props.monster.type1);
+    const attackingMatchups = state.data.typeMatchups.value.filter((typeMatchup) => typeMatchup.attackingType == props.monster.type1);
+    if (props.monster.type1 !== props.monster.type2) {
+        defendingMatchups.push(...state.data.typeMatchups.value.filter(typeMatchup => typeMatchup.defendingType == props.monster.type2));
+        attackingMatchups.push(...state.data.typeMatchups.value.filter(typeMatchup => typeMatchup.attackingType == props.monster.type2));
+    }
 
-    const defendingMatchups = state.data.typeMatchups.value.filter((typeMatchup) => typeMatchup.attackingType.includes(props.monster.type1)
-        || typeMatchup.attackingType.includes(props.monster.type2)).map(matchup => `${matchup.effectiveness} ${matchup.attackingType}` );
+    const computedAttackingMatchups = new Map<string, number>();
+    const computedDefendingMatchups = new Map<string, number>();
+    for(const matchup of attackingMatchups) {
+        const computedEffectiveness = GetTypeMatchupMultiplier(matchup);
+        if (computedAttackingMatchups.has(matchup.defendingType)) {
+            computedAttackingMatchups.set(matchup.defendingType, computedAttackingMatchups.get(matchup.defendingType) * computedEffectiveness);
+        } else {
+            computedAttackingMatchups.set(matchup.defendingType, computedEffectiveness);
+        }
+    }
+    for(const matchup of defendingMatchups) {
+        const computedEffectiveness = GetTypeMatchupMultiplier(matchup);
+        if (computedDefendingMatchups.has(matchup.attackingType)) {
+            computedDefendingMatchups.set(matchup.attackingType, computedDefendingMatchups.get(matchup.attackingType) * computedEffectiveness);
+        } else {
+            computedDefendingMatchups.set(matchup.attackingType, computedEffectiveness);
+        }
+    }
+
+    const formattedAttackingMatchups = [];
+    const formattedDefendingMatchups = [];
+    computedAttackingMatchups.forEach((effectiveness, type) => {
+        if (effectiveness !== 1) {
+            formattedAttackingMatchups.push(`${effectiveness}x against ${type}`);
+        }
+    });
+    computedDefendingMatchups.forEach((effectiveness, type) => {
+        if (effectiveness !== 1) {
+            formattedDefendingMatchups.push(`${type} does ${effectiveness}x`);
+        }
+    });
 
     return <div>
+        Type 1: {props.monster.type1}<br />
+        Type 2: {props.monster.type2}<br />
         Base Stats<br />
         HP: {props.monster.baseHP}<br />
         Attack: {props.monster.baseAttack}<br />
@@ -22,7 +58,7 @@ export const SidebarDetail = (props: SidebarDetailProps) => {
         Speed: {props.monster.baseSpeed}<br />
         Special: {props.monster.baseSpecial}<br />
         Growth Rate: {props.monster.growthRate}<br />
-        Attacking Matchups: {attackingMatchups.join(', ')}<br />
-        Defending Matchups: {defendingMatchups.join(', ')}<br />
+        Attacking Matchups: {formattedAttackingMatchups.join(', ')}<br />
+        Defending Matchups: {formattedDefendingMatchups.join(', ')}<br />
     </div>;
 };
--- src/domains/types/data/TypeMatchup.ts
+++ src/domains/types/data/TypeMatchup.ts
@@ -3,3 +3,18 @@ export interface TypeMatchup {
     defendingType: string;
     effectiveness: string;
 }
+
+export const GetTypeMatchupMultiplier = (typeMatchup: TypeMatchup) => {
+    switch (typeMatchup.effectiveness) {
+        case "NO_EFFECT":
+            return 0;
+        case "NOT_VERY_EFFECTIVE":
+            return 0.5;
+        case "EFFECTIVE":
+            return 1;
+        case "SUPER_EFFECTIVE":
+            return 2;
+        default:
+            throw new Error(`Unknown effectiveness: ${typeMatchup.effectiveness}`);
+    }
+} 
