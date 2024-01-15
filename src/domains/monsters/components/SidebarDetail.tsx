a0c972d093bad8cdb147ad299d6a747ea74a2f8c
alexjplant <alex@alexplant.org>
2024-01-15 14:47:26 -0800

    Format type matchups a bit better and extract func

--- src/domains/monsters/components/SidebarDetail.tsx
+++ src/domains/monsters/components/SidebarDetail.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "preact/hooks";
 import { State } from "../../../state";
 import { Monster } from "../data/Monster";
-import { GetTypeMatchupMultiplier } from "../../types/data/TypeMatchup";
+import { ComputeTypeMatchups, GetTypeMatchupMultiplier } from "../../types/data/TypeMatchup";
 
 export interface SidebarDetailProps {
     monster: Monster;
@@ -9,46 +9,10 @@ export interface SidebarDetailProps {
 
 export const SidebarDetail = (props: SidebarDetailProps) => {
     const state = useContext(State);
-    const defendingMatchups = state.data.typeMatchups.value.filter((typeMatchup) => typeMatchup.defendingType === props.monster.type1);
-    const attackingMatchups = state.data.typeMatchups.value.filter((typeMatchup) => typeMatchup.attackingType == props.monster.type1);
-    if (props.monster.type1 !== props.monster.type2) {
-        defendingMatchups.push(...state.data.typeMatchups.value.filter(typeMatchup => typeMatchup.defendingType == props.monster.type2));
-        attackingMatchups.push(...state.data.typeMatchups.value.filter(typeMatchup => typeMatchup.attackingType == props.monster.type2));
-    }
-
-    const computedAttackingMatchups = new Map<string, number>();
-    const computedDefendingMatchups = new Map<string, number>();
-    for(const matchup of attackingMatchups) {
-        const computedEffectiveness = GetTypeMatchupMultiplier(matchup);
-        if (computedAttackingMatchups.has(matchup.defendingType)) {
-            computedAttackingMatchups.set(matchup.defendingType, computedAttackingMatchups.get(matchup.defendingType) * computedEffectiveness);
-        } else {
-            computedAttackingMatchups.set(matchup.defendingType, computedEffectiveness);
-        }
-    }
-    for(const matchup of defendingMatchups) {
-        const computedEffectiveness = GetTypeMatchupMultiplier(matchup);
-        if (computedDefendingMatchups.has(matchup.attackingType)) {
-            computedDefendingMatchups.set(matchup.attackingType, computedDefendingMatchups.get(matchup.attackingType) * computedEffectiveness);
-        } else {
-            computedDefendingMatchups.set(matchup.attackingType, computedEffectiveness);
-        }
-    }
-
-    const formattedAttackingMatchups = [];
-    const formattedDefendingMatchups = [];
-    computedAttackingMatchups.forEach((effectiveness, type) => {
-        if (effectiveness !== 1) {
-            formattedAttackingMatchups.push(`${effectiveness}x against ${type}`);
-        }
-    });
-    computedDefendingMatchups.forEach((effectiveness, type) => {
-        if (effectiveness !== 1) {
-            formattedDefendingMatchups.push(`${type} does ${effectiveness}x`);
-        }
-    });
+    const {attackingMatchups, defendingMatchups} = ComputeTypeMatchups(state.data.typeMatchups.value, props.monster);
 
     return <div>
+        <span style={{fontWeight: 'bold'}}>{props.monster.name}</span><br />
         Type 1: {props.monster.type1}<br />
         Type 2: {props.monster.type2}<br />
         Base Stats<br />
@@ -58,7 +22,7 @@ export const SidebarDetail = (props: SidebarDetailProps) => {
         Speed: {props.monster.baseSpeed}<br />
         Special: {props.monster.baseSpecial}<br />
         Growth Rate: {props.monster.growthRate}<br />
-        Attacking Matchups: {formattedAttackingMatchups.join(', ')}<br />
-        Defending Matchups: {formattedDefendingMatchups.join(', ')}<br />
+        Attacking Matchups: {attackingMatchups.map(matchup => <span class='badge'>{matchup}</span>)}<br />
+        Defending Matchups: {defendingMatchups.map(matchup => <span class='badge'>{matchup}</span>)}<br />
     </div>;
 };
--- src/domains/types/data/TypeMatchup.ts
+++ src/domains/types/data/TypeMatchup.ts
@@ -1,3 +1,5 @@
+import { Monster } from "../../monsters/data/Monster";
+
 export interface TypeMatchup {
     attackingType: string;
     defendingType: string;
@@ -17,4 +19,53 @@ export const GetTypeMatchupMultiplier = (typeMatchup: TypeMatchup) => {
         default:
             throw new Error(`Unknown effectiveness: ${typeMatchup.effectiveness}`);
     }
-} 
+} ;
+
+export const ComputeTypeMatchups = (typeMatchups: TypeMatchup[], monster: Monster) => {
+    const defendingMatchups = typeMatchups.filter((typeMatchup) => typeMatchup.defendingType === monster.type1);
+    const attackingMatchups = typeMatchups.filter((typeMatchup) => typeMatchup.attackingType == monster.type1);
+    if (monster.type1 !== monster.type2) {
+        defendingMatchups.push(...typeMatchups.filter(typeMatchup => typeMatchup.defendingType == monster.type2));
+        attackingMatchups.push(...typeMatchups.filter(typeMatchup => typeMatchup.attackingType == monster.type2));
+    }
+
+    const computedAttackingMatchups = new Map<string, number>();
+    const computedDefendingMatchups = new Map<string, number>();
+    for(const matchup of attackingMatchups) {
+        const computedEffectiveness = GetTypeMatchupMultiplier(matchup);
+        if (computedAttackingMatchups.has(matchup.defendingType)) {
+            computedAttackingMatchups.set(matchup.defendingType, computedAttackingMatchups.get(matchup.defendingType) * computedEffectiveness);
+        } else {
+            computedAttackingMatchups.set(matchup.defendingType, computedEffectiveness);
+        }
+    }
+    for(const matchup of defendingMatchups) {
+        const computedEffectiveness = GetTypeMatchupMultiplier(matchup);
+        if (computedDefendingMatchups.has(matchup.attackingType)) {
+            computedDefendingMatchups.set(matchup.attackingType, computedDefendingMatchups.get(matchup.attackingType) * computedEffectiveness);
+        } else {
+            computedDefendingMatchups.set(matchup.attackingType, computedEffectiveness);
+        }
+    }
+
+    const formattedAttackingMatchups: string[] = [];
+    const formattedDefendingMatchups: string[] = [];
+    computedAttackingMatchups.forEach((effectiveness, type) => {
+        if (effectiveness !== 1) {
+            formattedAttackingMatchups.push(`${effectiveness}x vs. ${type}`);
+        }
+    });
+    computedDefendingMatchups.forEach((effectiveness, type) => {
+        if (effectiveness !== 1) {
+            formattedDefendingMatchups.push(`${type} does ${effectiveness}x`);
+        }
+    });
+    return {
+        attackingMatchups: formattedAttackingMatchups,
+        defendingMatchups: formattedDefendingMatchups
+    };
+};
+
+export const ComputeMonsterMatchup = (typeMatchups: TypeMatchup[], attacker: Monster, defender: Monster) => {
+    
+};
